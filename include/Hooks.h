7a2adeeb80c1d2feefc52ed09efb1236be204a93
RavenKZP <mkruk96@gmail.com>
2026-03-17 10:33:58 +0100

    Move to LinkObject, add HasWindModifier check
    After LoadBinary we have 0 modifiers.
    They are added during: LinkObject, so I moved the hook to LinkObject, and I'm adding my BSWindModifier only if no BSWindModifier is present.

--- include/Hooks.h
+++ include/Hooks.h
@@ -3,20 +3,32 @@
 
 namespace Hooks {
 
-    struct NiParticleSystem_AddModifier_Hook {
+    static bool HasWindModifier(RE::NiParticleSystem* ps) {
+        if (!ps) return false;
+        auto& list = ps->GetParticleSystemRuntimeData().modifierList;
+        for (const auto& mod : list) {
+            if (skyrim_cast<RE::BSWindModifier*>(mod.get())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    struct LinkObjectHook {
         static void Hook(RE::NiParticleSystem* a_this, RE::NiStream& a_stream) {
-            originalFunc_(a_this, a_stream);
-            static auto wind = RE::BSWindModifier::Create("ParticleWind", 10.0f);
-            if (!wind) return;
-            a_this->AddModifier(wind);
+            original(a_this, a_stream);
+            if (!HasWindModifier(a_this)) {
+                auto wind = RE::BSWindModifier::Create("ParticleWind", 10.0f);
+                if (!wind) return;
+                a_this->AddModifier(wind);
+            }
         }
 
-        static inline REL::Relocation<decltype(Hook)> originalFunc_;
+        static inline REL::Relocation<decltype(Hook)> original;
     };
 
     inline void InstallHooks() {
         REL::Relocation<std::uintptr_t> vtbl{RE::VTABLE_NiParticleSystem[0]};
-        NiParticleSystem_AddModifier_Hook::originalFunc_ =
-            vtbl.write_vfunc(0x18, NiParticleSystem_AddModifier_Hook::Hook);  // LoadBinary
+        LinkObjectHook::original = vtbl.write_vfunc(0x19, LinkObjectHook::Hook);
     }
 }
